ac616cc5c2041594a30df691b624ac4f5a3a4b8a
DaryaAniskevich <darya.aniskevich@mail.ru>
2024-10-07 18:04:43 +0300

    feat: zip

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,19 @@
+import fs from "node:fs";
+import zlib from "node:zlib";
+
 const compress = async () => {
-    // Write your code here 
+  const file = "./src/zip/files/fileToCompress.txt";
+
+  if (!fs.existsSync(file)) {
+    throw new Error("FS operation failed");
+  } else {
+    const readStream = fs.createReadStream(file);
+    const gzip = zlib.createGzip();
+
+    readStream
+      .pipe(gzip)
+      .pipe(fs.createWriteStream("./src/zip/files/archive.gz"));
+  }
 };
 
 await compress();
