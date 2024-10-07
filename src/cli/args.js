5817834f35beed3bf003e00b35f4f2341161dab1
DaryaAniskevich <darya.aniskevich@mail.ru>
2024-10-07 14:40:50 +0300

    feat: cli task

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,17 @@
 const parseArgs = () => {
-    // Write your code here 
+  const args = process.argv.slice(2);
+
+  let result = "";
+
+  for (let i = 0; i < args.length; i++) {
+    if (i % 2 === 0) {
+      result += args[i] + " is ";
+    } else {
+      result += args[i] + ", ";
+    }
+  }
+
+  console.log(result);
 };
 
 parseArgs();
